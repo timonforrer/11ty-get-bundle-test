3a193507133c2e0c8c9c64bb03673ea538e5ad5d
timonforrer <timon.forrer@gmail.com>
2024-01-25 14:46:27 +0100

    Remove unused code

--- .eleventy.js
+++ .eleventy.js
@@ -1,20 +1,13 @@
 require('dotenv').config();
 
-const { eleventyImagePlugin } = require('@11ty/eleventy-img');
-const EleventyPluginOGImage = require('eleventy-plugin-og-image');
 const { EleventyServerlessBundlerPlugin } = require('@11ty/eleventy');
-const fs = require('fs');
 const pluginWebc = require('@11ty/eleventy-plugin-webc');
-const { toHTML } = require('@portabletext/to-html');
-const { toPlainText } = require('./utils/toPlainText');
-const { renderScss } = require('./utils/renderScss');
 
 module.exports = function(config) {
   config.addPlugin(pluginWebc, {
     components: [
       'src/_includes/components/**/*.webc',
       'src/pages/**/*-page-handler.webc',
-      'npm:@11ty/eleventy-img/*.webc'
     ],
   });
 
@@ -23,35 +16,7 @@ module.exports = function(config) {
     functionsDir: './netlify/functions/',
     redirects: "netlify-toml-functions",
   });
-
-  config.addPlugin(eleventyImagePlugin, {
-    formats: ["webp", "jpeg"],
-		urlPath: "/img/",
-    defaultAttributes: {
-			loading: "lazy",
-			decoding: "async"
-		}
-  });
-
-  config.addPlugin(EleventyPluginOGImage, {
-    satoriOptions: {
-      fonts: [
-        {
-          name: 'Arial',
-          data: fs.readFileSync('./fonts/Arial.ttf'),
-          weight: 400,
-          style: 'normal'
-        }
-      ]
-    }
-  });
-
-  config.addFilter('toHTML', toHTML);
-  config.addFilter('toPlainText', toPlainText);
-
-  config.addTemplateFormats('scss');
-  config.addExtension('scss', renderScss(config));
-
+  
   return {
     dir: {
       input: './src/',
